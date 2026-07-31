40259eaf188492305048580efd1079c096904f93
Egor Mikulevich <mikulevicegor@gmail.com>
2026-07-31 11:20:04 +0300

    fix

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "sa_key_file" {
+    description = "sa key file"
+    type = string
+}
+
+variable "cloud_id" {
+  description = "cloud id"
+  type = string
+}
+
+variable "folder_id" {
+    description = "folder id"
+    type = string
+}
