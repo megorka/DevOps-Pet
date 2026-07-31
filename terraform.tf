459e488967db2fb9aea7eccc26b908a87a1e28e2
Egor Mikulevich <mikulevicegor@gmail.com>
2026-07-31 14:19:50 +0300

    Terraform applying pipeline

--- terraform.tf
+++ terraform.tf
@@ -4,6 +4,21 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  backend "s3" {
+    bucket = "devops-pet-tfstate"
+    region = "ru-central1"
+    key = "terraform.tfstate"
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_s3_checksum = true
+    skip_requesting_account_id = true
+    use_path_style = true
+  }
   required_version = ">= 0.13"
 }
 
